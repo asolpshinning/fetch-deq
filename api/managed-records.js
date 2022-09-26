64f06b45a19c21cd683023275fd52f1aadcaee0f
asolpshinning <asolpshinning14@gmail.com>
2022-09-26 17:24:26 -0500

    init before testing

---
+++ api/managed-records.js
@@ -0,0 +1,8 @@
+import URI from "urijs";
+
+// /records endpoint
+window.path = "http://localhost:3000/records";
+
+// Your retrieve function plus any additional functions go here ...
+
+export default retrieve;
